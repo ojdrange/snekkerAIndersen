cabd1e8d0b6dfb650a09b259cee5263625c29185
ojdrange <ole@akari.no>
2026-01-22 20:11:58 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   define: {
-    'process.env.API_KEY': JSON.stringify(process.env.VITE_API_KEY || process.env.API_KEY)
+    'process.env.API_KEY': JSON.stringify(process.env.VITE_API_KEY || process.env.API_KEY || '')
   },
   server: {
     port: 3000
